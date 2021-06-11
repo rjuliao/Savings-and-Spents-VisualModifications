0438eb038f0394d0965c22e275ed422094185591
rjuliao <raulj@uninorte.edu.co>
2021-06-11 17:49:07 -0500

    HTML and JS controller settings

--- force-app/main/default/lwc/fortnightInformationCmp/fortnightInformationCmp.js
+++ force-app/main/default/lwc/fortnightInformationCmp/fortnightInformationCmp.js
@@ -4,10 +4,11 @@ import TOTAL_FIXED_COSTS from '@salesforce/schema/Fortnight__c.Total_fixed_costs
 import TOTAL_PURCHASES from '@salesforce/schema/Fortnight__c.Total_purchases__c';
 import TOTAL_SPENT from '@salesforce/schema/Fortnight__c.Total_Spent__c';
 import { getFieldValue, getRecord } from 'lightning/uiRecordApi';
+import {NavigationMixin} from 'lightning/navigation';
 
 const fields = [NAME, TOTAL_FIXED_COSTS, TOTAL_PURCHASES, TOTAL_SPENT]
 
-export default class FortnightInformationCmp extends LightningElement {
+export default class FortnightInformationCmp extends NavigationMixin(LightningElement) {
     @api recordId
 
     //Get the record information
@@ -15,18 +16,35 @@ export default class FortnightInformationCmp extends LightningElement {
     fortnight
 
     editHandleBtm(){
-
+        this[NavigationMixin.Navigate]({
+            type : 'standard__recordPage',
+            attributes : {
+                recordId : this.recordId,
+                objectApiName : 'Fortnight__c',
+                actionName : 'edit',
+            }
+        })
     }
 
     backHandleBtm(){
-
+        this[NavigationMixin.Navigate]({
+            type : 'standard__objectPage',
+            attributes : {
+                objectApiName : 'Fortnight__c',
+                actionName : 'list',
+                filterName : 'All'
+            }
+        })
     }
 
     deleteHandleBtm(){
-        
+
     }
 
     //Get fields information
+    get getColorValue(){
+        return TOTAL_SPENT < 800000 ? 'value3' : TOTAL_SPENT >= 800000 && TOTAL_SPENT <1100000 ? 'value1' : 'value2';
+    }
 
     get nameValue(){
         return getFieldValue(this.fortnight.data, NAME)
