a5f93f634b4715d59876d12194ee9c052b871e81
rjuliao <raulj@uninorte.edu.co>
2021-06-11 10:12:40 -0500

    Base modification in styles

--- force-app/main/default/lwc/fortnightInformationCmp/fortnightInformationCmp.js
+++ force-app/main/default/lwc/fortnightInformationCmp/fortnightInformationCmp.js
@@ -1,3 +1,46 @@
-import { LightningElement } from 'lwc';
+import { api, LightningElement, wire } from 'lwc';
+import NAME from '@salesforce/schema/Fortnight__c.Name';
+import TOTAL_FIXED_COSTS from '@salesforce/schema/Fortnight__c.Total_fixed_costs__c';
+import TOTAL_PURCHASES from '@salesforce/schema/Fortnight__c.Total_purchases__c';
+import TOTAL_SPENT from '@salesforce/schema/Fortnight__c.Total_Spent__c';
+import { getFieldValue, getRecord } from 'lightning/uiRecordApi';
 
-export default class FortnightInformationCmp extends LightningElement {}
+const fields = [NAME, TOTAL_FIXED_COSTS, TOTAL_PURCHASES, TOTAL_SPENT]
+
+export default class FortnightInformationCmp extends LightningElement {
+    @api recordId
+
+    //Get the record information
+    @wire(getRecord, {recordId : '$recordId', fields})
+    fortnight
+
+    editHandleBtm(){
+
+    }
+
+    backHandleBtm(){
+
+    }
+
+    deleteHandleBtm(){
+        
+    }
+
+    //Get fields information
+
+    get nameValue(){
+        return getFieldValue(this.fortnight.data, NAME)
+    }
+
+    get totalFixedCost(){
+        return getFieldValue(this.fortnight.data, TOTAL_FIXED_COSTS)
+    }
+
+    get totalPurchases(){
+        return getFieldValue(this.fortnight.data, TOTAL_PURCHASES)
+    }
+
+    get totalSpents(){
+        return getFieldValue(this.fortnight.data, TOTAL_SPENT)
+    }
+}
